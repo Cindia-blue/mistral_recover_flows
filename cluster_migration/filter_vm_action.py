18eabc309d569f105128e01f72692e808cf165c8
Xinhui Li <lxinhui@vmware.com>
2017-01-21 20:09:04 +0800

    revise live migration

--- cluster_migration/filter_vm_action.py
+++ cluster_migration/filter_vm_action.py
@@ -1,7 +1,8 @@
 """
 FilterVmAction - custom action.
 
-Simple action for filtering VM on the presence of metadata "cluster_id"
+Simple action for filtering VM on the presence of metadata/extra spec
+"cluster" flag
 """
 from mistral.actions.openstack.actions import NovaAction
 from mistral.workflow.utils import Result
@@ -13,7 +14,8 @@ class FilterVmException(Exception):
 
 class FilterVmAction(NovaAction):
     """
-    Filter and return VMs whith the flag 'cluster_id' either on vm metadtata.
+    Filter and return VMs whith the flag 'cluster' either on vm metadtata
+    or flavor extra spec.
     """
 
     def __init__(self, metadata, flavor, uuid, cluster_id, node_id):
@@ -29,9 +31,10 @@ class FilterVmAction(NovaAction):
         client = self._get_client()
         metadata = self._metadata
 
-        if str(metadata.get('cluster_id')) == str(self._cluster_id):
+        if str(metadata.get('cluster')) == str(self._cluster_id):
             if str(self._uuid) == str(self._node_id):
-                hypervisor_hostname = client.servers.find(id=self._uuid).to_dict()['OS-EXT-SRV-ATTR:hypervisor_hostname']
-                return Result(data={'migrate': True, 'uuid': self._uuid, 'hypervisor_hostname': hypervisor_hostname})
+                #hypervisor_hostname = client.servers.find(id=self._uuid).to_dict()['OS-EXT-SRV-ATTR:hypervisor_hostname']
+                hypervisor_hostname = client.servers.find(id=self._uuid).to_dict()['OS-EXT-SRV-ATTR:host']
+                return Result(data={'migrate': True, 'uuid': self._uuid, 'hypervisor_hostname': hypervisor_hostname,'flavor_id': self._flavor})
 
-        return Result(data={'migrate': False, 'uuid': self._uuid, 'hypervisor_hostname': ""})
+        return Result(data={'migrate': False, 'uuid': self._uuid, 'hypervisor_hostname': '', 'flavor_id':self._flavor})
